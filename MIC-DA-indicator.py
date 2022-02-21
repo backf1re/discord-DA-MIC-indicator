d115e4a1696bd2d25ddbce93cc80875e805c79c5
backf1re <player.backfire@gmail.com>
2022-02-22 01:16:29 +0300

    initial

---
+++ MIC-DA-indicator.py
@@ -0,0 +1,90 @@
+import asyncio
+import json
+import socketio
+import pyfirmata
+import time
+import discord
+
+intents = discord.Intents.all()
+intents.voice_states = True
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+client = Bot(command_prefix="!")
+global counter
+counter = 0
+
+board = pyfirmata.Arduino('COM13')
+TOKEN = 'HSc6VtRWNdRupwqsKN1J'
+TOKEN_bot = 'NDkxMzcxNTAyNDk1NTk2NTQ0.W6AnXw.WZrG_xrzP2zdiIbO5iZm0XQSjdk'
+bot = commands.Bot(command_prefix='!')
+sio = socketio.Client()
+
+
+@bot.event
+async def on_ready():
+	print('Logged in...')
+
+
+@bot.event
+async def on_voice_state_update(self, before, after):
+	global counter
+	channel = 336533397696675841
+	if self.id == 336532034006810634 and after.self_mute == True:
+		counter = 0
+		board.digital[2].write(0)
+		time.sleep(1)
+		mystring = f'MIC t={counter}'
+		print(mystring)
+		board.digital[2].write(1)
+		print("MIC MUTE")
+
+	if self.id == 336532034006810634 and after.self_mute == False:
+		board.digital[2].write(0)
+		counter = 0
+		mystring = f'MIC t={counter}'
+		print(mystring)
+		print("MIC UNMUTE")
+	return (counter)
+
+@sio.on('connect')
+def on_connect():
+	sio.emit('add-user', {"token": TOKEN, "type": "alert_widget"})
+
+
+@sio.on('donation')
+def on_message(data):
+	global counter
+	y = json.loads(data)
+	# text = "New donate from: " + (y['username']), (y['amount']), (y['currency'])
+	text = "New alert about " + (y['username'])
+	channel = 336533397696675841
+	asyncio.run_coroutine_threadsafe(send_msg(channel, text), bot.loop)
+	counter = 1
+	mystring = f'Alert t={counter}'
+	# while True:
+	# 	print(mystring)
+	# 	time.sleep(5)
+	while counter == 1:
+		board.digital[2].write(1)
+		mystring = f'led on'
+		print(mystring)
+		time.sleep(1)
+		board.digital[2].write(0)
+		mystring = f'led off'
+		print(mystring)
+		mystring = f'Alert t={counter}'
+		print(mystring)
+		time.sleep(1)
+
+
+
+async def send_msg(channel, text):
+	channel = bot.get_channel(channel)
+	await channel.send(text)
+
+
+
+print("Что, кожанный мешок, Бота запустил, Бабок ждешь.... ну ну.... ну давай подождем.... ")
+sio.connect('wss://socket.donationalerts.ru:443', transports='websocket')
+bot.run(TOKEN_bot)
